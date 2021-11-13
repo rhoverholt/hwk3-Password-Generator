3291e5db16710444fbe0622bd7f78dbf4f0d6f89
rhoverholt <rhoverholt@gmail.com>
2021-11-12 20:33:20 -0500

    Expand comments and develop the README.md

--- script.js
+++ script.js
@@ -5,62 +5,74 @@ var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
-// and notify user if they canceled the pwd generation
+// and notify user if they aborted the pwd generation
 function writePassword() {
+
   var password = generatePassword();
+
   if (password != null) {
     var passwordText = document.querySelector("#password");
     passwordText.value = password;
   } else {
+    // Simple abort message for all error scenarios
     alert("Password Generation Canceled");
   }
 }
 
+/********************************************************************************************************/
 // Interact with user to determine password criteria and ultimately generate the password
 // INPUT: none
 // OUTPUT: null if the user aborted, or a valid password of the user-defined length with their charsets.
-// PROCESS: Will generate multiple modal questions for the user to answer 
+// USER INTERACTION: Will generate multiple modal questions for the user to answer 
+/********************************************************************************************************/
 function generatePassword() {
 
+  // First user interaction -- get the desired length of the password
   let pwdLength = getPwdLength();
 
   // If the user cancelled out of the length questions, abort password generation
   if (pwdLength === null) {return(null)};
 
+  // Second user interaction -- determine which character sets to include
   let charString = getCharString();
 
   // user must enter at least one charset.  If they did not, make them aware so they can abort or try again.
   while (charString === "") {
-    
+
     let wantContinue = confirm("At least one character set must be selected to continue.\nClick 'OK' to try again or 'Cancel' to abort.");
 
     if (wantContinue) {
+      // Try again
       charString = getCharString();
-    } else {
+    } else { // Abort at user's request.
       return(null);
     }
   }
 
   // Generate and return the actual password
   return getPassword(pwdLength, charString)
- 
-  //The end, the rest are all of the functions used
 
+  /********************************************************************************************************/
+  // The end, the rest are the internal functions used
+  /********************************************************************************************************/
+
+  /********************************************************************************************************/
   // This function will ask the user for a valid length for the password and
-  // handle all error scenarios, returning null whenever the user cancels or a valid integer
-  function getPwdLength() {
+  // handle all error scenarios, returning null whenever the user cancels or else the valid integer entered
+  /********************************************************************************************************/
+function getPwdLength() {
     
     const minPwdLength = 8;
     const maxPwdLength = 128;
     let pwdLength = prompt("How long should your password be (8-128):", 10);
 
-    // While they haven't entered an integer in the range of 8-128
-    // Note, this loop allows them to cancel twice at any point to leave the generation process.
+    // While they haven't entered an integer in the range of 8-128, allow the user to try again.
+    // Make them cancel twice to abort the generation process.
     while (!(pwdLength >= minPwdLength && pwdLength <= maxPwdLength && pwdLength == parseInt(pwdLength,10))) {
 
       // Handle user request to cancel password generation
       if (pwdLength === null) {
-          pwdLength = prompt("Press cancel to cancel the password generation process or enter a password length to continue (8-128):", 10);
+          pwdLength = prompt("Press cancel to abort the password generation process or enter a password length to continue (8-128):", 10);
       }
 
       // Cancel and return if user truly chooses to cancel
@@ -78,6 +90,11 @@ function generatePassword() {
     return(pwdLength);
   }
 
+  /********************************************************************************************************/
+  // OUTPUT: This function returns a string of valid characters to include in the password or null to abort
+  // INPUT: None
+  // User Interface: Multiple modal messages are sent to the user to determine which characters to include
+  /********************************************************************************************************/
   function getCharString() {
 
     const lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
@@ -99,6 +116,7 @@ function generatePassword() {
 
     return(outputString);
 
+    // A deeply nested function to avoid the ugliness of repeating it 4 times above.
     function allowCharSet(setName, setDetails) {
       return (confirm(`\n
 ${setName} (${setDetails})\n
@@ -107,8 +125,31 @@ Press 'Cancel' to not allow them in your password.\n`)
       )
     }
   }
+
+  /********************************************************************************************************/
+  // INPUT: The desired length and the possible chars to include
+  // OUTPUT: A string of the desired length including random characters from the list
+  // User Interface: None
+  /********************************************************************************************************/
   function getPassword (pwdLength, passwordChars) {
 
+    // Validate the input data, aborting if it is invalid
+    // pwdLength must be a number that is 1 or greater
+    if (!(typeof pwdLength === 'number') && (isNaN(pwdLength)) && (pwdLength >= 1)) {
+      
+      console.log (typeof pwdLength);
+
+      console.log (typeof pwdLength === 'number');
+      console.log(isNAN(pwdLength));
+      console.log(pwdLength >= 1);
+
+      return null;
+    }
+
+    // passwordChars must be a non-empty string
+    if (!(typeof passwordChars === 'string')) {return null}
+
+    // Input is valid, so process through and create the output Password.
     Password = "";
 
     while (Password.length < pwdLength) {
@@ -118,17 +159,3 @@ Press 'Cancel' to not allow them in your password.\n`)
     return Password;
   }
 }
-/*
-function random_password_generate(min,max)
-{
-    var passwordChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#@!%&()/";
-    var randPwLen = Math.floor(Math.random() * (max - min + 1)) + min;
-    var randPassword = Array(randPwLen).fill(passwordChars).map(function(x) { return x[Math.floor(Math.random() * x.length)] }).join('');
-    return randPassword;
-}
-
-document.getElementById("generatePassword").addEventListener("click", function(){
-    random_password = random_password_generate(16,8);
-    document.getElementById("randomPassword").value = random_password;
-}); 
-*/
